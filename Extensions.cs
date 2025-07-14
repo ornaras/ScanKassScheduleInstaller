2ea9aee20bf330649725c9602902bf30b81f33f8
ornaras <ornaras.us@gmail.com>
2025-07-14 11:52:37 +0400

    Убрано автополучение ссылки на скачивание архива

--- Extensions.cs
+++ Extensions.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Win32;
-using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -29,26 +27,6 @@ namespace ScanKass
             catch (Exception e)
             {
                 SchedulerInstaller.LogError("Скачивание было принудительно остановлено!", e);
-                return null;
-            }
-        }
-
-        internal static async Task<string> GetLatestReleaseAsync(this HttpClient http)
-        {
-            SchedulerInstaller.LogInfo("Получение ссылки на последнюю версию планировщика...");
-            try
-            {
-                var req = new HttpRequestMessage(HttpMethod.Get, Constants.UrlGitLatest);
-                req.Headers.Add("accept", "application/vnd.github+json");
-                req.Headers.Add("User-Agent", Constants.UserAgent);
-                var resp = await http.SendAsync(req);
-                var body = await resp.Content.ReadAsStringAsync();
-                var json = JArray.Parse(body);
-                return json[0]["assets"][0]["browser_download_url"].Value<string>();
-            }
-            catch (Exception e)
-            {
-                SchedulerInstaller.LogError("Не удалось получить ссылку на последнюю версию планировщика!", e);
                 return null;
             }
         }
